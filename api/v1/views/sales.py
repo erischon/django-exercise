3d7754e58bdbb9aeb12f6e5ed9639fe223bf8a3d
Eri Schon <erischon@gmail.com>
2022-03-18 19:07:12 +0100

    Update sales.py

--- api/v1/views/sales.py
+++ api/v1/views/sales.py
@@ -3,12 +3,29 @@ from sales.models import Article, ArticleCategory, Sale
 from api.v1.serializers.sales import SaleSerializer, ArticleSerializer
 
 
+class CreateArticle(generics.CreateAPIView):
+    serializer_class = ArticleSerializer
+
+
 class ListSale(generics.ListAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
 
-class CreateArticle(generics.CreateAPIView):
-    serializer_class = ArticleSerializer
 
 class CreateSale(generics.CreateAPIView):
     serializer_class = SaleSerializer
+
+
+class PutSale(generics.UpdateAPIView):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+
+
+class DeleteSale(generics.DestroyAPIView):
+    queryset = Sale.objects.all()
+    serializer_class = SaleSerializer
+    # permission_classes = [IsAuthenticated]
+
+    # def get_queryset(self):
+    #     queryset = Sale.objects.filter(author = self.request.user, id = self.kwargs['pk'])
+    #     return queryset
