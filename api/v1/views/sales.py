9cbcd2bceeadf2fd3a66f454ebbc75af54028b82
Eri Schon <erischon@gmail.com>
2022-03-19 20:13:06 +0100

    Update sales.py

--- api/v1/views/sales.py
+++ api/v1/views/sales.py
@@ -1,6 +1,8 @@
 from django.db.models import Prefetch
 
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import SessionAuthentication
 
